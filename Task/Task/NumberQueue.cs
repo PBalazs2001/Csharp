e3ed187adcf07b88061e128ea7cb2e10f14f0082
PBalazs2001 <79253120+PBalazs2001@users.noreply.github.com>
2022-05-08 22:52:22 +0200

    Add files via upload

---
+++ Task/Task/NumberQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task
+{
+    internal class NumberQueue
+    {
+        private ConcurrentQueue<int> _queue;
+
+        public NumberQueue(ConcurrentQueue<int> queue)
+        {
+            Queue = queue;
+        }
+
+        public ConcurrentQueue<int> Queue { get => _queue; set => _queue = value; }
+    }
+}
